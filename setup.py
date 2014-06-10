6da310c25083fc0f4d94691bc0ee35588262a2f7
Henry Gomersall <heng@cantab.net>
2014-06-10 12:58:12 +0100

    Now builds for 64-bit drivers.

--- setup.py
+++ setup.py
@@ -21,40 +21,103 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from distutils.util import get_platform
+from distutils.ccompiler import get_default_compiler
 
 import os
 import numpy
+import sys
 
+try:
+    from Cython.Distutils import build_ext as build_ext
+    pico4k_sources = [os.path.join('picopy', 'pico4k.pyx')]
+    pico_status_sources = [os.path.join('picopy', 'pico_status.pyx')]
+except ImportError:
+    # We can't cythonize, but that might have been done already in a
+    # source distribution, so carry on and see...
+    from distutils.command.build_ext import build_ext
+    pico4k_sources = [os.path.join('picopy', 'pico4k.c')]
+    pico_status_sources = [os.path.join('picopy', 'pico_status.c')]
 
-include_dirs = [numpy.get_include()]
+include_dirs = [os.path.join('include', '4k'), numpy.get_include()]
 library_dirs = []
 package_data = {}
 
-if get_platform() == 'win32':
-    #libraries = ['PS3000a']
+if get_platform() in ('win32', 'win-amd64'):
     libraries = ['PS4000']
-    include_dirs.append('picopy')
-    include_dirs.append(os.path.join('include', '4k'))
+    #include_dirs.append(os.path.join('include', 'win'))
     library_dirs.append(os.path.join(os.getcwd(),'picopy'))
-    package_data['picopy'] = ['PS4000.dll', 'PicoIpp.dll']
+    package_data['picopy'] = ['PS4000.dll', 'PicoIpp.dll', 'PS4000.lib']
 else:
     libraries = ['ps3000a', 'usb_pico-1.0']
 
+class custom_build_ext(build_ext):
+    def finalize_options(self):
+
+        build_ext.finalize_options(self)
+
+        if self.compiler is None:
+            compiler = get_default_compiler()
+        else:
+            compiler = self.compiler
+
+        if compiler == 'msvc':
+            # Add msvc specific hacks
+            
+            if (sys.version_info.major, sys.version_info.minor) < (3, 3):
+                # The check above is a nasty hack. We're using the python
+                # version as a proxy for the MSVC version. 2008 doesn't
+                # have stdint.h, so is needed. 2010 does.
+                #
+                # We need to add the path to msvc includes
+                include_dirs.append(os.path.join('include', 'msvc_2008'))
+
+            # We need to prepend lib to all the library names
+            _libraries = []
+            for each_lib in self.libraries:
+                _libraries.append('lib' + each_lib)
+
+            self.libraries = _libraries
 
 ext_modules = [
-    #Extension('picopy.pico3k',
-    #        sources=[os.path.join('picopy', 'pico3k.c')],
-    #        libraries=libraries,
-    #        library_dirs=library_dirs),
-    Extension('picopy.pico4k',
-            sources=[os.path.join('picopy', 'pico4k.c')],
+        Extension('picopy.pico4k',
+            sources=pico4k_sources,
             libraries=libraries,
             include_dirs=include_dirs,
             library_dirs=library_dirs),
-    Extension(
+        Extension(
             'picopy.pico_status',
-            sources = [os.path.join('picopy', 'pico_status.c')], 
+            sources = pico_status_sources, 
             include_dirs=include_dirs)]
+        
+#include_dirs = [numpy.get_include()]
+#library_dirs = []
+#package_data = {}
+#
+#if get_platform() == 'win32':
+#    #libraries = ['PS3000a']
+#    libraries = ['PS4000']
+#    include_dirs.append('picopy')
+#    include_dirs.append(os.path.join('include', '4k'))
+#    library_dirs.append(os.path.join(os.getcwd(),'picopy'))
+#    package_data['picopy'] = ['PS4000.dll', 'PicoIpp.dll', 'PS4000.lib']
+#else:
+#    libraries = ['ps3000a', 'usb_pico-1.0']
+#
+#
+#ext_modules = [
+#    #Extension('picopy.pico3k',
+#    #        sources=[os.path.join('picopy', 'pico3k.c')],
+#    #        libraries=libraries,
+#    #        library_dirs=library_dirs),
+#    Extension('picopy.pico4k',
+#            sources=[os.path.join('picopy', 'pico4k.c')],
+#            libraries=libraries,
+#            include_dirs=include_dirs,
+#            library_dirs=library_dirs),
+#    Extension(
+#            'picopy.pico_status',
+#            sources = [os.path.join('picopy', 'pico_status.c')], 
+#           include_dirs=include_dirs)]
 
 version = '0.0.1'
 
@@ -83,6 +146,7 @@ setup_args = {
         'ext_modules': ext_modules,
         'include_dirs': include_dirs,
         'package_data': package_data,
+        'cmdclass': {'build_ext': custom_build_ext},
   }
 
 if __name__ == '__main__':
